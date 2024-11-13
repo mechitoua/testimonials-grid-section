e92a5ff6d999c6490ed3b2f1fb4218e3c7a724f9
mehmet_emin <mechitoua.m.amine@outlook.sa>
2024-11-13 18:33:29 +0100

    adding testimonials data in a separate file

---
+++ src/data.ts
@@ -0,0 +1,53 @@
+export const testimonials = [
+  {
+    name: 'Daniel Clifford',
+    title: 'Verified Graduate',
+    quote:
+      "I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny's worth.",
+    content:
+      "I was an EMT for many years before I joined the bootcamp. I've been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I've successfully switched careers, working as a Software Engineer at a VR startup.",
+    avatarUrl:
+      'https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=150&h=150&fit=crop',
+    className: 'bg-moderate-violet text-white col-span-2 md:col-span-2',
+  },
+  {
+    name: 'Jonathan Walters',
+    title: 'Verified Graduate',
+    quote: 'The team was very supportive and kept me motivated',
+    content:
+      "I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I've made in myself.",
+    avatarUrl:
+      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=150&h=150&fit=crop',
+    className: 'bg-dark-grayish-blue text-white',
+  },
+  {
+    name: 'Kira Whittle',
+    title: 'Verified Graduate',
+    quote: 'Such a life-changing experience. Highly recommended!',
+    content:
+      "Before joining the bootcamp, I've never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint.",
+    avatarUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=150&h=150&fit=crop',
+    className: 'bg-white text-dark-grayish-blue md:row-span-2',
+  },
+  {
+    name: 'Jeanette Harmon',
+    title: 'Verified Graduate',
+    quote: 'An overall wonderful and rewarding experience',
+    content:
+      'Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love.',
+    avatarUrl:
+      'https://images.unsplash.com/photo-1580489944761-15a19d654956?w=150&h=150&fit=crop',
+    className: 'bg-white text-dark-grayish-blue',
+  },
+  {
+    name: 'Patrick Abrams',
+    title: 'Verified Graduate',
+    quote: 'Awesome teaching support from TAs who did the bootcamp themselves',
+    content:
+      'The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people.',
+    avatarUrl:
+      'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=150&h=150&fit=crop',
+    className: 'bg-dark-blackish-blue text-white col-span-2 md:col-span-2',
+  },
+];
