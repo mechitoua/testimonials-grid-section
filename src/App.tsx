fe6a250087a6f333c7ad5c3dd23240e01a5da94e
mehmet_emin <mechitoua.m.amine@outlook.sa>
2024-11-15 17:47:38 +0100

    fix: adjusting the data used by the card component & extending tailwind colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-function App() {
+import TestimonialCard from './componenets/TestimonialCard';
+import { testimonials } from './data';
+
+export default function App() {
   return (
-    <>
-      <h1 className='text-center text-4xl font-bold'>
-        Testimonials Grid Section
-      </h1>
-    </>
+    <div className='flex justify-center items-center min-h-screen bg-light-grayish-blue px-6 py-20 h-screen'>
+      <div className='mx-auto max-w-7xl'>
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4'>
+          {testimonials.map((testimonial, index) => (
+            <TestimonialCard key={index} {...testimonial} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'moderate-violet': 'hsl(263, 55%, 52%)',
+        'dark-grayish-blue': 'hsl(217, 19%, 35%)',
+        'dark-blackish-blue': 'hsl(219, 29%, 14%)',
+        'light-gray': 'hsl(0, 0%, 81%)',
+        'light-grayish-blue': 'hsl(210, 46%, 95%)',
+      },
+    },
   },
   plugins: [],
 };
